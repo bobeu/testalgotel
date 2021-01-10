e3e470c840d6c081cf887a6d2e67b6e377ca0c54
bobeu <bobmatea27@gmail.com>
2021-01-10 02:46:16 +0100

    initial commit

---
+++ buyToken.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from algosdk.future.transaction import transaction
+from algosdk import mnemonic
+from waitforconfirmation import wait_for_confirmation
+from algosdk.future.transaction import AssetTransferTxn
+from client import connect
+from getInput import markup2
+from optIn import optin
+import logging
+import os
+from dotenv import load_dotenv
+from telegram.ext import ConversationHandler
+import time
+
+algod_client = connect(None, None)
+load_dotenv()
+price_progress = [1000, ]  # Tracks price change
+current_price = price_progress[-1]
+saleable = 6000000  # Total asset available for IPO.
+total_buy = 0
+assetBalance = 0
+asset_id = 13251912
+
+
+# Don't expose sensitive information.
+TO = os.getenv('DEFAULT2_ACCOUNT') #
+auth = os.getenv('DEFAULT2_MNEMONTIC') #
+rate = 30
+
+# pull account information of supposedly contract account
+accountInfo = algod_client.account_info(TO)
+successful = None
+
+
+def response_end(update, context, message):
+    """
+    Update the user with response with recourse to the executing function
+    :param update:
+    :param context:
+    :param message: Message to forward to user --> str
+    :return:
+    """
+    update.message.reply_text(message, reply_markup=markup2)
+    return ConversationHandler.END
+
+
+# Price updater.
+# For every
+def updatePrice(update, context):
+    """
+    Update the price after every 2000000 token is sold
+        : price is increased by the
+    half of the current price
+    :param update:
+    :param context:
+    :return:
+    """
+    global rate
+    global current_price
+
+    if saleable >= 4000000:
+        rate = rate
+        current_price = current_price
+        update.message.reply_text(
+            "Current price per DMT2: {} MicroAlgo".format(current_price))
+    elif 2000000 < saleable < 4000000:
+        rate = 20
+        newPrice = int(current_price + (current_price / 2))
+        current_price = newPrice
+        price_progress.append(newPrice)
+        update.message.reply_text(
+            "Current price per DMT2: {} MicroAlgo".format(newPrice))
+    elif saleable <= 2000000:
+        rate = 10
+        newPrice = int(current_price + (current_price / 2))
+        current_price = newPrice
+        update.message.reply_text(
+            "Current price per DMT2: {} MicroAlgo".format(newPrice))
+
+
+def updateAssetBalance(update, context):
+    """
+    Continious update of asset balance in creator's account
+    NB: We will need this for a check
+    :param update:
+    :param context:
+    :return:
+    """
+    global assetBalance
+    for i in accountInfo['assets']:
+        if i['asset-id'] == asset_id:
+            assetBalance = i['amount']
+            break
+
+
+def transfer(update, context, receiver, amount):
+    """
+    Transfer a custom asset from default account A to account B (Any)
+    :param update: Default telegram argument
+    :param context: Same as update
+    :param sender: Sender of this transaction
+    :param receiver: The beneficiary of the asset in transit
+    :param amount: Amount in custom asset other than ALGO
+    :return:
+    """
+    global saleable
+    params = algod_client.suggested_params()
+    params.fee = 1000
+    params.flat_fee = True
+    try:
+        assert saleable >= amount, response_end(
+            update, context, "Sales for the period is exhausted")
+
+        txn = AssetTransferTxn(
+            sender=TO,  # asset_manage_authorized,
+            sp=params,
+            receiver=receiver,
+            amt=int(amount),
+            index=asset_id
+        )
+        # Sign the transaction
+        sk = mnemonic.to_private_key(auth)
+        signedTrxn = txn.sign(sk)
+
+        # Submit transaction to the network
+        tx_id = algod_client.send_transaction(signedTrxn)
+        message = "Successful! \nTransaction hash: {}.".format(tx_id)
+        wait_for_confirmation(update, context, algod_client, tx_id)
+        logging.info(
+            "...##Asset Transfer... \nReceiving account: {}.\nMessage: {}\nOperation: {}\nTxn Hash: {}"
+            .format(receiver, message, transfer.__name__, tx_id))
+
+        update.message.reply_text(message, reply_markup=markup2)
+        saleable -= amount
+        # Erase the key soon as you're done with it.
+        context.user_data.clear()
+    except Exception as err:
+        print(err)
+    return markup2
+
+
+def buy_token(update, context):
+    """
+    Purchase an ASA: This function handles the whole computations, takes
+    payment from the user and forward token remission to "transfer()"
+    :param update:
+    :param context:
+    :return:
+    """
+    # Request to modify global variables. Peculiar to
+    # Python 3 or newer.
+    global successful
+    global current_price
+    global price_progress
+    global rate
+
+    # Extracts the arguments from the temp_DB
+    user_data = context.user_data
+    buyer = user_data["Public_key"]
+    qty = user_data["Quantity"]
+    sk = user_data["Secret_Key"]
+    note = user_data["Note"].encode()
+
+    updatePrice(update, context)
+    updateAssetBalance(update, context)
+
+    # Firstly, opt in user if not already subscribe to DMT2 asset.
+    optin(update, context, buyer, sk)
+
+    max_buy = 500000  # Instant purchase pegged to 500,000 DMT2
+
+    params = algod_client.suggested_params()
+    fee = params.fee = 1000
+    flat_fee = params.flat_fee = True
+    amount_to_pay = int(current_price * qty)
+    alc_bal = algod_client.account_info(buyer)['amount']
+
+    assert alc_bal > amount_to_pay, response_end(update, context, "Not enough balance.")
+    assert qty <= max_buy, response_end(update, context, "Max amount per buy is restricted to 500000 DMT2.")
+    assert qty >= 500, response_end(
+        update, context, "Minimum buy is 500 DMT2.\n Session ended.")
+    assert len(buyer) == 58, response_end(update, context,
+                                          "Incorrect address")
+
+    # If there is enough token to sell
+    # Perform other necessary checks, then execute if all passed.
+
+    try:
+        time.sleep(4)
+        if saleable > 0:
+            update.message.reply_text("Broadcasting transaction...")
+
+            raw_trxn = transaction.PaymentTxn(sender=buyer,
+                                              fee=fee,
+                                              first=params.first,
+                                              last=params.last,
+                                              gh=params.gh,
+                                              receiver=TO,
+                                              amt=amount_to_pay,
+                                              close_remainder_to=None,
+                                              note=note,
+                                              gen=params.gen,
+                                              flat_fee=flat_fee,
+                                              lease=None,
+                                              rekey_to=None)
+
+            # Sign the transaction
+            signedTrxn = raw_trxn.sign(sk)
+            update.message.reply_text("Just a second.....")
+            # Submit transaction to the network
+            tx_id = algod_client.send_transaction(signedTrxn)
+            message = "Transaction hash: {}.".format(tx_id)
+            wait = wait_for_confirmation(update, context, algod_client, tx_id)
+            logging.info(
+                "...##Asset Transfer... \nReceiving account: {}.\nMessage: {}\nOperation: {}\n"
+                .format(buyer, message, buy_token.__name__))
+            successful = bool(wait is not None)
+
+            if successful:
+                amountToSend = qty + (qty * (rate / 100))
+                update.message.reply_text(
+                    f"Payment success...\nTransferring {amountToSend} token to address... --> {buyer}",
+                    reply_markup=markup2
+                )
+                transfer(update, context, buyer, amountToSend)
+            else:
+                response_end(update, context, "Transaction declined")
+        else:
+            update.message.reply_text("Token unavailable at the moment", reply_markup=markup2)
+
+    except Exception as err:
+        logging.info("Error encountered: ".format(err))
+        update.message.reply_text("Something went wrong1\nTransaction Unsuccessful", reply_markup=markup2)
+
---
+++ getInput.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+from typing import Dict
+
+from telegram import ReplyKeyboardMarkup, Update
+from telegram.ext import CallbackContext
+from telegram.ext import ConversationHandler
+
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+STARTING, GETPK, GETMNEMONIC, ACCOUNTBAL, GETALCSTAT, TYPING_REPLY_2 = range(6)
+
+reply_keyboard2 = [
+    ['/Mnemonic'],
+    ['/GetNow', '/Cancel']
+]
+
+reply_keyboard3 = [
+    ['/Private_key'],
+    ['/Getnow', '/Cancel']
+]
+
+reply_keyboard4 = [
+    ['/Public_key'],
+    ['/GetnoW', '/Cancel']
+]
+
+reply_keyboard5 = [
+    ['/Public_Key'],
+    ['/GetNoW', '/Cancel']
+]
+
+
+reply_keyboard_category = [
+    ['/Buy_DMT2', '/Get_PK', '/Account_balance'],
+    ['/GetMnemonic', '/Get_Alc_status', '/GetAlc'],
+    ['/About', '/Help', '/Cancel', '/Main_Menu'],
+]
+
+
+reply_keyboard_main = [
+    ['/GetAlc'],
+    ['/Buy_DMT2', '/Others'],
+    ['/Cancel'],
+]
+
+keyboard = [
+    ['/Get_PK', '/GetMnemonic'],
+    ['/Account_balance', '/Get_Alc_status'],
+    ['/Main_menu'],
+]
+
+mappedKeyboard = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+
+
+markup_category = ReplyKeyboardMarkup(reply_keyboard_category, one_time_keyboard=True)
+markup2 = ReplyKeyboardMarkup(reply_keyboard_main, one_time_keyboard=True)
+markup3 = ReplyKeyboardMarkup(reply_keyboard2, one_time_keyboard=True)
+markup4 = ReplyKeyboardMarkup(reply_keyboard3, one_time_keyboard=True)
+markup5 = ReplyKeyboardMarkup(reply_keyboard4, one_time_keyboard=True)
+markup6 = ReplyKeyboardMarkup(reply_keyboard5, one_time_keyboard=True)
+
+user_d = {}
+
+
+def facts_to_str(user_data: Dict[str, str]) -> str:
+    arg = list()
+
+    for key, value in user_data.items():
+        arg.append(f'{key} - {value}')
+
+    return "\n".join(arg).join(['\n', '\n'])
+
+
+def getMarkup(update, context, message, mark_up):
+    update.message.reply_text(message, reply_markup=mark_up)
+
+
+def main_menu(update, context):
+    update.message.reply_text(
+        "Select an option :\n\nBe sure you have enough "
+        "balance in your account before trying to buy DMT2 token.\n"
+        "Firstly, get a testnet account if you don't have one.\n"
+        "Remember testnet ALGOs have no monetary value.\n\n"
+        "Click /Others to do other things.",
+        reply_markup=markup2,
+    )
+
+
+def inputcateg(update: Update, context: CallbackContext) -> int:
+    update.message.reply_text(
+        "Select an option from the list...",
+        reply_markup=markup_category,
+    )
+
+    return STARTING
+
+
+def select_choice(update, context) -> int:
+    text = update.message.text
+    if text == '/Get_PK':
+        update.message.reply_text(
+            "You want to convert Mnemonic to private key?\n"
+            "I'll need the '25 words', Click on Mnemonic.",
+            reply_markup=markup3,
+        )
+        return GETPK
+
+    elif text == '/GetMnemonic':
+        update.message.reply_text(
+            "You want to convert private key to Mnemonic?\n"
+            "I'll need the 'private key', Click on Private_key.",
+            reply_markup=markup4,
+        )
+        return GETMNEMONIC
+
+    elif text == 'Account_balance' or text == '/Account_balance':
+        update.message.reply_text(
+            "You want to check the balance on your account?\n"
+            "I'll need the 'Public key', Click on Public_key.",
+            reply_markup=markup5,
+        )
+        return ACCOUNTBAL
+
+    elif text == '/Get_Alc_status':
+        update.message.reply_text(
+            "You want to check the status of your account?\n"
+            "I'll need the 'Public key', Click on Private-key.",
+            reply_markup=markup6,
+        )
+        return GETALCSTAT
+
+
+def otherwise(update, context):
+    text = update.message.text
+    expected = [
+        '/Mnemonic',
+        '/Private_key',
+        '/Public_key',
+        '/Public_Key'
+    ]
+    for b in expected:
+        if text == b:
+            user_d[b] = text
+            update.message.reply_text(f'Now give the {text.lower()}?')
+
+    return TYPING_REPLY_2
+
+
+def response(update, context, mark_up, command):
+    text = update.message.text
+    update.message.reply_text(
+        "I got this argument:\n"
+        f"{text}\n"
+        f"If you're sure, click /{command} to execute.",
+        reply_markup=mark_up,
+    )
+
+
+def helper_func(
+        update,
+        context,
+        category,
+        text,
+        markup,
+        command
+):
+
+    user_d[category] = text
+    response(update, context, markup, command)
+    user_data = context.user_data
+    user_data[category] = user_d[category]
+
+
+def received_information_2(update, context):
+    text = update.message.text
+    for a in user_d:
+        category = user_d[a]
+        if category == '/Mnemonic':
+            helper_func(update, context, category, text, markup3, "GetNow")
+            return GETPK
+
+        elif category == '/Private_key' and len(text) > 58:
+            helper_func(update, context, category, text, markup4, "Getnow")
+            return GETMNEMONIC
+
+        elif category == '/Public_key' and len(text) == 58:
+            helper_func(update, context, category, text, markup5, "GetnoW")
+            return ACCOUNTBAL
+
+        elif category == '/Public_Key' and len(text) == 58:
+            helper_func(update, context, category, text, markup6, "GetNoW")
+            return GETALCSTAT
+        else:
+            update.message.reply_text('Wrong argument is supplied', reply_markup=mappedKeyboard)
+
+    user_d.clear()
+    return STARTING
+
+
+# Returns to main menu
+def menuKeyboard(update, context):
+    update.message.reply_text('Select')
+    inputcateg(update, context)
+    return STARTING
+
+
+# def alternative(update, context):
+#     update.message.reply_text("Navigate with command:", reply_markup=markup7)
+#
+
+def done(update: Update, context: CallbackContext):
+    return ConversationHandler.END
